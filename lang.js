da1d086f598974532d8f928e60928d8bfd2f6e82
kirbIndustries <189890658+kirbIndustries@users.noreply.github.com>
2025-09-14 10:36:15 +0100

    Create lang.js

---
+++ lang.js
@@ -0,0 +1,23 @@
+const LANG_EN_WEATHER = {
+    LOADING_ERROR: "Failed to load weather"
+};
+
+const LANG_JP_WEATHER = {
+    LOADING_ERROR: "天気の読み込みに失敗しました"
+};
+
+const LANG_DE_WEATHER = {
+    LOADING_ERROR: "Wetter konnte nicht geladen werden"
+};
+
+const LANG_ES_WEATHER = {
+    LOADING_ERROR: "No se pudo cargar el tiempo"
+};
+
+const LANG_KO_WEATHER = {
+    LOADING_ERROR: "날씨를 불러오지 못했습니다"
+};
+
+const LANG_ZH_WEATHER = {
+    LOADING_ERROR: "加载天气失败"
+};
